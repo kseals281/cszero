ae6b0b732d470bd84be0b90182b82d08ceabffb8
kseals281 <kseals281@gmail.com>
2014-11-20 23:06:35 -0500

    changed image

--- Khari/image.py
+++ Khari/image.py
@@ -3,3 +3,13 @@ image = [[w, w, w, w, w],
          [w, w, g, w, w],
          [w, b, w, b, w],
          [w, w, w, w, w]]
+         
+def removeRed(image):
+    newImage = []
+    for row in image:
+        newRow = []
+        for pixel in row:
+            newRow.append(0, pixel[1], pixel[2])
+            #255 - pixel[0], 255 - pixel[1], 255- pixel[2] inversion
+        newImage.append(newRow)
+    return newImage
